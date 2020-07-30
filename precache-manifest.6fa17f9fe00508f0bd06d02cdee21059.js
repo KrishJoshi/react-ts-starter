65add1285dbc849f8602bf35a139da7f573645d6
Krish Joshi <krish.k.joshi@gmail.com>
2020-07-30 02:59:24 +0100

    Updates

---
+++ precache-manifest.6fa17f9fe00508f0bd06d02cdee21059.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeeb064b37f942bc318a5d7755c7c566",
+    "url": "/index.html"
+  },
+  {
+    "revision": "865c1dd6a25b59feacca",
+    "url": "/static/css/main.ef27c204.chunk.css"
+  },
+  {
+    "revision": "e8085533a351a4870505",
+    "url": "/static/js/2.564c3118.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.564c3118.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "865c1dd6a25b59feacca",
+    "url": "/static/js/main.0b71f38a.chunk.js"
+  },
+  {
+    "revision": "668b2a22446ca678e927",
+    "url": "/static/js/runtime-main.f7832a02.js"
+  }
+]);
